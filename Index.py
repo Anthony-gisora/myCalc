179621c6e0c9eae339cf1e0a42cab7c4428046bb
Anthony-gisora <99672074+Anthony-gisora@users.noreply.github.com>
2022-09-23 13:58:34 +0300

    Update Index.py

--- Index.py
+++ Index.py
@@ -1,2 +1,24 @@
 Print("welcome to this app. Ready for some calculation...")
-oper = input("Choose an operation: ")
+def get_values(a=0,b=0,c=0,d=0, oper):
+  if oper == "+":
+    ans == a + b + c + d
+    print(f"{a} + {b} + {c} + {d} = {ans}")
+  elif oper == "-":
+    ans = a - b - c - d
+    print(f"{a} - {b} - {c} - {d} = {ans}")
+  elif oper == "*":
+    ans = a * b * c * d
+    print(f"{a} * {b} * {c} * {d} = {ans}")
+  elif oper == "/":
+    print("system under constructuin")
+  else:
+    print("invalid entry")
+  
+op = input("Choose an operation: 1) +\n 2) -\n 3) *\n 4) / \n")
+a = input("Enter a number: ")
+b = input("Enter a number: ")
+c = input("Enter a number: ")
+d = input("Enter a number: ")
+
+get_values(a,b,c,d, op)
+
